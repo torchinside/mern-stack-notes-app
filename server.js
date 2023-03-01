8351f9deb33421eac10efe958cc4f6dc66bf5867
Jose Luis Fleitas <joseft11294@gmail.com>
2023-03-01 01:23:17 -0500

    add logs handler middleware and date-fns and uuid dependencies

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3500;
 const path = require('path');
 
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(express.json());
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use('/', require('./routes/root'));
 
